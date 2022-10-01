fbf30445d67624588463b1a3e593289bb6eacaa6
DindhaAchmadMaulana <114462755+DindhaAchmadMaulana@users.noreply.github.com>
2022-10-01 09:46:26 +0700

    Add files via upload

---
+++ Pratikum_01/src/Latihan07.java
@@ -0,0 +1,10 @@
+public class Latihan07 {
+	
+	public static void main(String[] args) {
+		
+		String data = "Dindha Achmad M";
+		data = data.replaceAll("[A,a,i,u,e,o]", "X");
+		System.out.println("Dindha Achmad M ==>" + data);
+	}	    
+
+}
---
+++ Pratikum_01/src/Latihan08.java
@@ -0,0 +1,21 @@
+public class Latihan08 {
+	
+	public static void main(String[] args) {
+		
+		String NamaLengkap, NamaKelas;
+		int NPM, Semester;
+		Double IPK;
+		
+		NPM = 212310050;
+		NamaLengkap = "Dindha Achmad Maulana";
+		NamaKelas = "TI-21-PA1";
+		Semester = 3;
+		IPK = 3.6;
+		
+		System.out.println("NPM : " + NPM);
+		System.out.println("Nama Lengkap : " + NamaLengkap);
+		System.out.println("Nama Kelas : " + NamaKelas);
+		System.out.println("Semester : " + Semester);
+		System.out.println("IPK : " + IPK);
+	}
+}
